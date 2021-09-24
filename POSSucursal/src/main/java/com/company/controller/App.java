776ef87b10f5a2ea9c5d96fb6efd0c90d781a6a9
eduardo sanchez <e.gsanchez@outlook.com>
2021-09-24 16:32:13 -0500

    otro cambio en el titulo br

--- POSSucursal/src/main/java/com/company/controller/App.java
+++ POSSucursal/src/main/java/com/company/controller/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
-        stage.setTitle("Sistema");
+        stage.setTitle("Sistema de ventas kawai");
         stage.setResizable(false);
         stage.show();
     }
