a494dc53916cdaef95017c58da11237834cfe491
aldhair <aldhair.hdz.soto@gmail.com>
2021-09-29 17:12:36 -0500

    asdafg (se me acabaron los nombres)

---
+++ POSSucursal/src/main/java/ConnectionDB/ConnDBH2.java
@@ -0,0 +1,40 @@
+package ConnectionDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javafx.scene.control.Alert;
+
+public class ConnDBH2 {
+    
+    // Libreria de MySQL
+    public String driver = "org.h2.Driver";
+    //Ruta de la bd
+    public String url = "jdbc:h2:tcp://localhost/~/test";
+    // Nombre de usuario
+    public String userName = "sa";
+    // Contraseña
+    public String password = "";
+    
+    // Metodo de conexion
+    public Connection connectionDbH2(){
+        
+        Connection conn = null;
+        
+        try {
+            
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, userName, password);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            
+            System.out.println(e.toString());
+            e.printStackTrace();
+            
+        }
+        
+        return conn;
+    
+    }
+    
+}
