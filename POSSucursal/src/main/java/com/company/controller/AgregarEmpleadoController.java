3fdb1afda8b4a1c3e16f4cc982a9af86ea3fc0b9
aldhair <aldhair.hdz.soto@gmail.com>
2021-10-01 12:33:55 -0500

    View add new employee, complete

--- POSSucursal/src/main/java/com/company/controller/AgregarEmpleadoController.java
+++ POSSucursal/src/main/java/com/company/controller/AgregarEmpleadoController.java
@@ -2,15 +2,12 @@ package com.company.controller;
 
 import ConnectionDB.ConnDBH2;
 
-import com.company.controller.App;
-import com.company.controller.App;
-import com.company.controller.PrincipalController;
-import com.company.controller.PrincipalController;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,10 +16,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class AgregarEmpleadoController implements Initializable {
@@ -33,11 +33,12 @@ public class AgregarEmpleadoController implements Initializable {
     @FXML private ImageView imageMain;
     @FXML private TextField txtUsuario;
     @FXML private TextField txtContrasena;
-    @FXML private TextField txtContrasena2;
+    @FXML private PasswordField txtContrasena2;
     @FXML private RadioButton radioAdmin;
     @FXML private RadioButton radioEmpleado;
     @FXML private Button btnRegistrarEmpleado;
     @FXML private Button btnCancelarRegistro;
+    private Button btnSee;
     
     // Agrupar radio buttons
     ToggleGroup tg = new ToggleGroup();
@@ -48,17 +49,25 @@ public class AgregarEmpleadoController implements Initializable {
 
     // Query
     private static String querySql = "";
+    
+    // Declaracion de la alerta
+    Alert alert = new Alert(Alert.AlertType.NONE);
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+        // Cargar la hora 
         PrincipalController.dateAndHour(this.txtDate);
+        
+        // Cargar la imagen
+        Image img = new Image("/images/image-main.png");
+        this.imageMain.setImage(img);
         
         this.radioAdmin.setToggleGroup(this.tg);
         this.radioEmpleado.setToggleGroup(this.tg);
+        this.radioAdmin.setSelected(true);
         
-        //this.txtContrasena.set
-
     }
 
     @FXML
@@ -74,61 +83,89 @@ public class AgregarEmpleadoController implements Initializable {
                 if (this.txtContrasena.getText().equals(this.txtContrasena2.getText())) {
 
                     // Crear el query
-                    querySql = "INSERT INTO usersemployees(userName, password) VALUES(?,?)";
+                    querySql = "INSERT INTO useremployee(user, password, typeEmployee) VALUES(?,?,?)";
                     
                     try {
-                    
+                        
                         //PreparedStatment
                         //bucar para que sirve esto
                         PreparedStatement preparedStatement = conn.prepareStatement(querySql);
                         preparedStatement.setString(1, this.txtUsuario.getText());
                         preparedStatement.setString(2, this.txtContrasena.getText());
-                        preparedStatement.execute();
-                        //conn.close();
+             
+                        // Obtener el tipo de emplado (Administrador / Empleado)
+                        if(this.radioAdmin.isSelected()) {
 
-                        System.out.println("datos almacenados");
+                            preparedStatement.setString(3, "Administrador");
+
+                        } else {
+
+                            preparedStatement.setString(3, "Empleado");
+
+                        }
+
+                        this.alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        this.alert.setTitle("Nuevo empleado");
+                        this.alert.setContentText("¿Desea agregar un nuevo usuario al sistema?");
+                        this.alert.setHeaderText(null);
+                        // Se agrega esto para obtener el resultado de la alerta de la confirmación
+                        Optional<ButtonType> action = this.alert.showAndWait();
+                       
+                        // confirmacion de la alerta
+                        if(action.get() == ButtonType.OK) {
+                            
+                            this.alert = new Alert(Alert.AlertType.INFORMATION);
+                            this.alert.setTitle("Nuevo empleado");
+                            this.alert.setContentText("¡Usuario agregado con exito!");
+                            this.alert.setHeaderText(null);
+                            this.alert.showAndWait();
+                            
+                            // Ejecutar el query 
+                            preparedStatement.execute();
+                            
+                            // Limpiar los campos
+                            this.txtUsuario.setText("");
+                            this.txtContrasena.setText("");
+                            this.txtContrasena2.setText("");
+                            this.radioAdmin.setSelected(true);
+                            
+                        } else {
+                            
+                            preparedStatement.cancel();
+                            System.out.println("no execute");
+                            
+                        }
                     
                     } catch (SQLException e) {
                     
                         System.out.println("Algo fallo: " + e.toString());
-                        //bucar para que sirve esto
+                        //buscar para que sirve esto
                         Logger.getLogger(AgregarEmpleadoController.class.getName()).log(Level.SEVERE, null, e);
                     
                     }
 
                 } else {
 
-                    Alert alert = new Alert(Alert.AlertType.WARNING);
-                    alert.setTitle("Nuevo empleado");
-                    alert.setContentText("La contraseña no coinciden, verificalo por favor");
-                    alert.setHeaderText(null);
-                    alert.showAndWait();
+                    this.alert = new Alert(Alert.AlertType.WARNING);
+                    this.alert.setTitle("Nuevo empleado");
+                    this.alert.setContentText("La contraseña no coinciden, verificalo por favor");
+                    this.alert.setHeaderText(null);
+                    this.alert.showAndWait();
 
                 }
 
             } else {
                 
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setTitle("Nuevo empleado");
-                alert.setContentText("Existe campos vacios");
-                alert.setHeaderText(null);
-                alert.showAndWait();
+                this.alert = new Alert(Alert.AlertType.WARNING);
+                this.alert.setTitle("Nuevo empleado");
+                this.alert.setContentText("Existe campos vacios");
+                this.alert.setHeaderText(null);
+                this.alert.showAndWait();
                 
             }
 
         }
 
-        /*
-        try {
-            App.setRoot("VistaListaEmpleado");
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        for(TextField item: CleanTextField){
-            item.setText = "";
-        }
-        
-         */
     }
 
     @FXML
