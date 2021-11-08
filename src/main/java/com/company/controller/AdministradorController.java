645a93be160ff03bbf4edaf82eb9960f51e9bbbc
eduardo sanchez <e.gsanchez@outlook.com>
2021-11-08 11:06:01 -0600

    conexión con API funcionado

--- src/main/java/com/company/controller/AdministradorController.java
+++ src/main/java/com/company/controller/AdministradorController.java
@@ -1,4 +1,3 @@
-
 package com.company.controller;
 
 import Configurations.DataAndHour;
@@ -26,143 +25,157 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
-import javax.xml.ws.http.HTTPException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
 public class AdministradorController implements Initializable {
 
-    @FXML private TableView<Sale> tbSalesAdmin;
-    @FXML private Button btnFilterUsers;
-    @FXML private ComboBox<String> cBoxUsers;
-    @FXML private Button btnSignOutAdmin;
-    @FXML private Button btnLoadJson;
-    @FXML private TextField txtSales;
-    @FXML private Button btnAddModUser;
-    @FXML private Label lblTitle1;
-    @FXML private Label lblDescription1;
-    @FXML private TextField txtDate;
-    @FXML private ImageView imageMain;
-    @FXML private TableColumn<Sale, Long> colIdSale;
-    @FXML private TableColumn<Sale, Integer> colIdEmployee;
-    @FXML private TableColumn<Sale, Integer> colIdBranchOffice;
-    @FXML private TableColumn<Sale, Double> colIdTotalSale;
-    @FXML private TableColumn<Sale, String> coldescription;
-    @FXML private TableColumn<Sale, String> colDate;
-    
+    @FXML
+    private TableView<Sale> tbSalesAdmin;
+    @FXML
+    private Button btnFilterUsers;
+    @FXML
+    private ComboBox<String> cBoxUsers;
+    @FXML
+    private Button btnSignOutAdmin;
+    @FXML
+    private Button btnLoadJson;
+    @FXML
+    private TextField txtSales;
+    @FXML
+    private Button btnAddModUser;
+    @FXML
+    private Label lblTitle1;
+    @FXML
+    private Label lblDescription1;
+    @FXML
+    private TextField txtDate;
+    @FXML
+    private ImageView imageMain;
+    @FXML
+    private TableColumn<Sale, Long> colIdSale;
+    @FXML
+    private TableColumn<Sale, Integer> colIdEmployee;
+    @FXML
+    private TableColumn<Sale, Integer> colIdBranchOffice;
+    @FXML
+    private TableColumn<Sale, Double> colIdTotalSale;
+    @FXML
+    private TableColumn<Sale, String> coldescription;
+    @FXML
+    private TableColumn<Sale, String> colDate;
+
     private ObservableList<Sale> listSales;
     private BufferedReader bufferedReader;
     private StringBuilder stringBuilder;
-    private String line;        	
+    private String line;
     private URL url;
-    
- 
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         DataAndHour.dateAndHour(this.txtDate);
         LoadImage.loadImageMain(this.imageMain);
-        
+
         this.colIdSale.setCellValueFactory(new PropertyValueFactory<>("id_sale"));
         this.colIdEmployee.setCellValueFactory(new PropertyValueFactory<>("id_employee"));
         this.colIdBranchOffice.setCellValueFactory(new PropertyValueFactory<>("id_branch_office"));
         this.colIdTotalSale.setCellValueFactory(new PropertyValueFactory<>("total_sale"));
         this.coldescription.setCellValueFactory(new PropertyValueFactory<>("description"));
         this.colDate.setCellValueFactory(new PropertyValueFactory<>("date_sale"));
-        
+
         listSales = tbSalesAdmin.getItems();
-   
-        
-    }    
+
+    }
 
     @FXML
-    private void filterUsers(ActionEvent event) { }
+    private void filterUsers(ActionEvent event) {
+    }
 
     @FXML
     private void signOutAdmin(ActionEvent event) {
-        
+
         Object ev = event.getSource();
-        
-        if(ev.equals(this.btnSignOutAdmin)){
-            
+
+        if (ev.equals(this.btnSignOutAdmin)) {
+
             try {
 
-            App.setRoot("VistaPrincipal");
+                App.setRoot("VistaPrincipal");
 
             } catch (IOException e) {
 
                 System.out.println("Error: " + e.getMessage());
 
             }
-            
+
         }
-        
+
     }
 
     @FXML
     private void addModUser(ActionEvent event) {
         try {
-            
+
             App.setRoot("VistaListaEmpleado");
-            
+
         } catch (IOException e) {
-            
+
             System.out.println("Error: " + e.getMessage());
-            
+
         }
-        
+
     }
 
     @FXML
     private void loadDataApi(ActionEvent event) throws IOException {
-        
+
         Object ev = event.getSource();
-        
-        if(ev.equals(this.btnLoadJson)){
-            
-            try { 
-            
-            url = new URL("http://localhost:9001/listar");
-            
-            //realiza la conexion
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");            
-            connection.connect();
-            
-            if(connection.getResponseCode() == 200){  
-                
-                System.out.println("Response: OK");
-                //obtiene respuesta
-                bufferedReader  = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                stringBuilder = new StringBuilder();
 
-                while ((line = bufferedReader.readLine()) != null) {
+        if (ev.equals(this.btnLoadJson)) {
 
-                    stringBuilder.append(line);
+            try {
+
+                url = new URL("http://localhost:9001/listar");
+
+                //realiza la conexion
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                if (connection.getResponseCode() == 200) {
+
+                    System.out.println("Response: OK");
+                    //obtiene respuesta
+                    bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    stringBuilder = new StringBuilder();
+
+                    while ((line = bufferedReader.readLine()) != null) {
+
+                        stringBuilder.append(line);
+
+                    }
+
+                    JSONArray dataArray = new JSONArray(stringBuilder.toString());
+                    tbSalesAdmin.getItems().removeAll(listSales);
+
+                    for (int i = 0; i < dataArray.length(); i++) {
+
+                        JSONObject row = dataArray.getJSONObject(i);
+                        listSales.add(new Sale(row.getLong("id_sale"), row.getInt("id_employee"), row.getInt("id_branch_office"), row.getDouble("total_sale"), row.getString("description"), row.getString("date_sale")));
+
+                    }
 
                 }
-
-                JSONArray dataArray  = new JSONArray(stringBuilder.toString());
-                tbSalesAdmin.getItems().removeAll(listSales);
-
-                for(int i = 0 ; i < dataArray.length(); i++){
-                    
-                    JSONObject row = dataArray.getJSONObject(i); 
-                    listSales.add(new Sale(row.getLong("id_sale"), row.getInt("id_employee"), row.getInt("id_branch_office"), row.getDouble("total_sale"), row.getString("description"), row.getString("date_sale")));
-
-                }
-
-            }
 
             } catch (MalformedURLException e) {
 
-                    e.printStackTrace();
+                e.printStackTrace();
 
             }
-            
+
         }
-        
+
     }
-    
+
 }
